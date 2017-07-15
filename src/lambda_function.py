07b2709033ec1ef99a0cd04632cbe752785baa09
h4llow3En <mail@felixdoering.com>
2017-07-15 10:04:20 +0200

    add needed default responses, fixing existing ones

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -22,12 +22,12 @@ def lambda_handler(request_obj, context=None):
 @alexa.default_handler()
 def default_handler(request):
     """ The default handler gets invoked if no handler is set for a request """
-    return alexa.create_response(message="Was kann ich für dich tun?")
+    return alexa.create_response(message="Was kann ich für dich tun?", end_session=False)
 
 
 @alexa.request_handler("LaunchRequest")
 def launch_request_handler(request):
-    return alexa.create_response(message="Was kann ich für dich tun?")
+    return alexa.create_response(message="Du kannst mich nach den Abfahrten an einer Haltestelle der d.v.b. fragen. Was kann ich für dich tun?", end_session=False)
 
 
 @alexa.request_handler("SessionEndedRequest")
@@ -35,8 +35,26 @@ def session_ended_request_handler(request):
     return
 
 
+@alexa.intent_handler("AMAZON.CancelIntent")
+def session_cancel_intent(request):
+    return alexa.create_response(message="Okay", end_session=True)
+
+
+@alexa.intent_handler("AMAZON.StopIntent")
+def session_stop_intent(request):
+    return alexa.create_response(message="Okay", end_session=True)
+
+
+@alexa.intent_handler('AMAZON.HelpIntent')
+def get_help_intent_handler(request):
+    response_text = "Frage einfach nach den Abfahrten an einer Haltestelle der d.v.b., \
+wie zum Beispiel 'Nenne mir die nächsten Abfahrten am Hauptbahnhof' \
+oder sage Stop um den Skill zu beenden. Was kann ich für dich tun?"
+    return alexa.create_response(message=response_text, end_session=False)
+
+
 @alexa.intent_handler('StationIntent')
-def get_recipe_intent_handler(request):
+def get_station_intent_handler(request):
     """
     You can insert arbitrary business logic code here
     """
@@ -46,8 +64,8 @@ def get_recipe_intent_handler(request):
     station = request.slots.get("station")
 
     if station is None:
-        return alexa.create_response(message="Entschuldigung, aber du hast \
-mir keine Haltestelle genannt.")
+        return alexa.create_response(message="Entschuldigung, du hast mir \
+keine Haltestelle genannt. Kannst du bitte deine Anfrage mit Haltestelle Wiederholen?")
 
     stops = dvb_monitor(station)
 
